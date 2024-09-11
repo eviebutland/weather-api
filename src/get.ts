603add233277dda4d71473710418ea57b9f0fa46
Evie perren <evie.butland@gmail.com>
2024-09-11 07:48:58 +0100

    Set values into redis for remaining endpoints

--- src/get.ts
+++ src/get.ts
@@ -43,6 +43,7 @@ export async function getWeatherToday(redis: FastifyRedis, request: FastifyReque
                 'snow',  weather.days[0].snow, 
                 'feelslike', weather.days[0].feelslike,
                 'uvindex',  weather.days[0].uvindex)
+
             return reply.status(200).send(weather)
         }
        
@@ -64,20 +65,39 @@ export async function getWeatherTomorrow(redis: FastifyRedis, request: FastifyRe
      
         const tomorrow = new Date()
         tomorrow.setUTCDate(new Date().getUTCDate() + 1)
+        const shorthandTomorrow = tomorrow
 
+        const storedTemp = await redis.hget(`${request.query?.city}:${shorthandTomorrow}`, 'temp')
+       
+        if(storedTemp){
+            const tomorrow = await redis.hgetall(`${request.query?.city}:${shorthandTomorrow}`)
+            
+            return reply.status(200).send({message: 'Collected from stored value', tomorrow})
+        } else {
 
-        const response = await getWeather(tomorrow.toISOString(), request.query?.city, request.getEnvs()?.WEATHER_API_KEY)
-    
-        if(response.status === 400){
-            return reply.status(400).send({error: 'Bad request, please check your city'})
-        } 
-
-        if(response.status === 500){
-            return reply.status(500).send({error: 'Something went wrong, please try again later'})
-        }
+            const response = await getWeather(tomorrow.toISOString(), request.query?.city, request.getEnvs()?.WEATHER_API_KEY)
         
-        const weather = await response.json()
-        return reply.status(200).send(weather)
+            if(response.status === 400){
+                return reply.status(400).send({error: 'Bad request, please check your city'})
+            } 
+
+            if(response.status === 500){
+                return reply.status(500).send({error: 'Something went wrong, please try again later'})
+            }
+            
+            const weather = await response.json()
+
+            await redis.hset(`${request.query?.city}:${shorthandTomorrow}`, 
+                'temp', weather.days[0].temp, 
+                'description', weather.days[0].description,
+                'tempmax', weather.days[0].tempmax, 
+                'precip', weather.days[0].precip, 
+                'snow',  weather.days[0].snow, 
+                'feelslike', weather.days[0].feelslike,
+                'uvindex',  weather.days[0].uvindex)
+
+            return reply.status(200).send(weather)
+        }
     } catch(error) {
         console.log(error)
         return reply.status(400).send(error)
@@ -85,7 +105,6 @@ export async function getWeatherTomorrow(redis: FastifyRedis, request: FastifyRe
 }
 
 export async function getWeatherConditionToday(redis: FastifyRedis, request: FastifyRequest, reply: FastifyReply){
-    // check redis first
     try {
         if (!request.query?.city) {
             return reply.status(400).send({message: 'Please provide a city'})
@@ -96,39 +115,59 @@ export async function getWeatherConditionToday(redis: FastifyRedis, request: Fas
         }
 
         const today = new Date().toISOString()
+        const shorthandToday = today.split('T')[0]
 
-        const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${request.query?.city}/${today}?unitGroup=metric&include=days&key=${request.getEnvs()?.WEATHER_API_KEY}&contentType=json`, {
-            "method": "GET",
-        })
-        
-        const weather = await response.json()
-        const todaysWeather = weather.days[0]
+        const storedTemp = await redis.hget(`${request.query?.city}:${shorthandToday}`, 'temp')
+       
+        if(storedTemp){
+            const today = await redis.hgetall(`${request.query?.city}:${shorthandToday}`)
+            
+            return reply.status(200).send({message: 'Collected from stored value', today})
+        } else {
 
-        if(request.query?.weatherCondition === 'snow'){
-            return todaysWeather.snow > 0 ? reply.status(200).send({snowDepth: todaysWeather.snowDepth, 
-                conditions: `${todaysWeather.conditions}. ${todaysWeather.description}`}) 
-                : reply.status(200).send({conditions:  `${todaysWeather.conditions}. ${todaysWeather.description}`})
-        }
+            const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${request.query?.city}/${today}?unitGroup=metric&include=days&key=${request.getEnvs()?.WEATHER_API_KEY}&contentType=json`, {
+                "method": "GET",
+            })
+            
+            const weather = await response.json()
+            const todaysWeather = weather.days[0]
 
-        if(request.query?.weatherCondition === 'rain'){
-            return todaysWeather.precipprob > 0 ? 
-                reply.status(200).send({
-                    precip: todaysWeather.precip, 
-                    precipprob: `${todaysWeather.precipprob}%`,
-                    conditions: `${todaysWeather.conditions}. ${todaysWeather.description}`
-                }) : reply.status(200).send({conditions: `${todaysWeather.conditions}. ${todaysWeather.description}`, 
-                    precipprob: `${todaysWeather.precipprob}%`,})
-        }
+            await redis.hset(`${request.query?.city}:${shorthandToday}`, 
+                'temp', todaysWeather.temp, 
+                'description', todaysWeather.description,
+                'tempmax', todaysWeather.tempmax, 
+                'precip', todaysWeather.precip, 
+                'snow',  todaysWeather.snow, 
+                'feelslike', todaysWeather.feelslike,
+                'uvindex',  todaysWeather.uvindex)
 
-        if(request.query.weatherCondition === 'sunshine'){
-            return todaysWeather.cloudcover < 30 ? reply.status(200).send({
-                conditions: `${todaysWeather.conditions}. ${todaysWeather.description}`, 
-                uvindex: todaysWeather.uvindex, 
-                cloudcover: todaysWeather.cloudcover, 
-                solarradiation: todaysWeather.solarradiation}) 
-                : reply.status(200).send({
+            if(request.query?.weatherCondition === 'snow'){
+                return todaysWeather.snow > 0 ? reply.status(200).send({snowDepth: todaysWeather.snowDepth, 
+                    conditions: `${todaysWeather.conditions}. ${todaysWeather.description}`}) 
+                    : reply.status(200).send({conditions:  `${todaysWeather.conditions}. ${todaysWeather.description}`})
+            }
+
+            if(request.query?.weatherCondition === 'rain'){
+                return todaysWeather.precipprob > 0 ? 
+                    reply.status(200).send({
+                        precip: todaysWeather.precip, 
+                        precipprob: `${todaysWeather.precipprob}%`,
+                        conditions: `${todaysWeather.conditions}. ${todaysWeather.description}`
+                    }) : reply.status(200).send({conditions: `${todaysWeather.conditions}. ${todaysWeather.description}`, 
+                        precipprob: `${todaysWeather.precipprob}%`,})
+            }
+
+            if(request.query.weatherCondition === 'sunshine'){
+                return todaysWeather.cloudcover < 30 ? reply.status(200).send({
                     conditions: `${todaysWeather.conditions}. ${todaysWeather.description}`, 
-                    precipprob: `${todaysWeather.precipprob}%`,})
+                    uvindex: todaysWeather.uvindex, 
+                    cloudcover: todaysWeather.cloudcover, 
+                    solarradiation: todaysWeather.solarradiation}) 
+                    : reply.status(200).send({
+                        conditions: `${todaysWeather.conditions}. ${todaysWeather.description}`, 
+                        precipprob: `${todaysWeather.precipprob}%`,})
+            }
+            
         }
     } catch (error) {
         console.log(error)
